ef550a62d114f1e6347228e4765b543aff5df43d
Marko Zecevic <marczecevic@gmail.com>
2021-06-20 09:07:57 +0200

    Updates

---
+++ precache-manifest.c304cdf06380ce14ff90a65ca90365a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b383a4c071c0374618cdd7d7696739fd",
+    "url": "/MarkoNS1990.github.io/index.html"
+  },
+  {
+    "revision": "9364c5e256ceb4a2bd76",
+    "url": "/MarkoNS1990.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/MarkoNS1990.github.io/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/MarkoNS1990.github.io/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9364c5e256ceb4a2bd76",
+    "url": "/MarkoNS1990.github.io/static/js/main.4a05b571.chunk.js"
+  },
+  {
+    "revision": "d3c0b1147adbe62cbb07",
+    "url": "/MarkoNS1990.github.io/static/js/runtime-main.26109960.js"
+  }
+]);
