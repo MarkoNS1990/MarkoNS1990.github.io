8e9bddb2a3a7e2b6338ab4f1808263d341f80eb3
Marko Zecevic <marczecevic@gmail.com>
2021-06-22 08:41:05 +0200

    Updates

---
+++ precache-manifest.b8475aa6e03711d6682db796abafe0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ca9f5d151fe250cf45c1db5a429aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a9adb03e93d35b1ae5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a9adb03e93d35b1ae5",
+    "url": "/static/js/main.b627e3f8.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
